1ad303884f8064069f3711fe24f00b34322032e9
Atharv Chandratre <atharvchandratre@gmail.com>
2023-09-29 22:36:40 -0500

    Adding files used to learn from the tutorials

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 export default props =>
-  <form>
-    <input
-      type='text'
-      className="new-todo"
-      placeholder="What needs to be done?"/>
-  </form>
+    <form onSubmit={props.handleTodoSubmit}>
+        <input
+            type='text'
+            autoFocus
+            value={props.currentTodo}
+            onChange={props.handleNewTodoChange}
+            className="new-todo"
+            placeholder="What needs to be done?"/>
+    </form>
